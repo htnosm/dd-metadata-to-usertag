77676460c69c2696395d471de6356d80f31e6e74
htnosm <htnosm@gmail.com>
2020-07-24 18:17:32 +0900

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # dd-metadata-to-usertag
 Add Datadog host's metadata to user tag
+
+## Description
+
+* Preserve existing user tags
+* Tag key with the same name updates the value
+* Specify the metadata to be tags in include_keys.yml
+    * metadata can be referenced from [Infrastructure] -> [Infrastructure List] -> "JSON API permalink"
+
+## Requirements
+
+* python 3.8+
+* [DataDog/datadogpy: The Datadog Python library](https://github.com/DataDog/datadogpy)
+* PyYAML
+
+## Installation
+
+```
+virtualenv -p python3 .venv
+. ./.venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+cd dd_add_metadata/
+
+export DD_API_KEY=""
+export DD_APP_KEY=""
+
+# Help
+python dd_add_metadata.py -h
+# DryRun
+python dd_add_metadata.py -d
+# Update
+python dd_add_metadata.py
+```
---
+++ dd_add_metadata/dd_add_metadata.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import yaml
+import argparse
+from datadog import initialize, api
+from operator import itemgetter
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser(description='')
+
+    parser.add_argument('-f', '--filter', type=str,
+                        help='query to filter search results', default="")
+    parser.add_argument('-c', '--conf-yaml', type=str,
+                        help='define yaml specify the metadata', default="./include_keys.yml")
+    parser.add_argument('-d', '--dryrun', action='store_true',
+                        help='without actually update, and output update plan')
+    return(parser.parse_args())
+
+
+def select_metadata(include_keys, metadata):
+    result = []
+    for include_key in include_keys.keys():
+        if include_key in metadata:
+            include_key_type = type(include_keys[include_key])
+            if include_key_type is dict:
+                user_tags = select_metadata(
+                    include_keys[include_key], metadata[include_key])
+                result.extend(user_tags)
+            elif include_key_type is list:
+                print(f"\"{include_key}\" list type not supported")
+            else:
+                user_tag = f"{include_keys[include_key]}:{metadata[include_key]}"
+                result.append(user_tag)
+    return result
+
+
+def main(filter="", conf_yaml="./include_keys.yml", dryrun=False):
+    with open(conf_yaml, 'r') as yml:
+        include_keys = yaml.safe_load(yml)
+
+    initialize()
+    res = api.Hosts.search(filter=filter)
+
+    host_list = []
+    for host in res['host_list']:
+        host['user_tag_count'] = 0
+        if 'tags_by_source' in host:
+            if 'Users' in host['tags_by_source']:
+                host['user_tag_count'] = len(host['tags_by_source']['Users'])
+        host_list.append(host)
+    host_list.sort(key=itemgetter('user_tag_count'))
+
+    for host in host_list:
+        # name == Datadog's host
+        datadogs_host = host['name']
+        print(f"# host: {datadogs_host}")
+
+        metadata = select_metadata(include_keys, host)
+        print(f"metadata: {metadata}")
+
+        before_tags = host['tags_by_source']['Users'] if host['user_tag_count'] > 0 else [
+        ]
+        before_tags.sort()
+        print(f"before: {before_tags}")
+
+        update_tags = metadata
+        for before_tag in before_tags:
+            preserve = True
+            if ":" in before_tag:
+                before_tag_key = before_tag.split(':')[0] + ":"
+            else:
+                before_tag_key = before_tag
+            for update_tag in update_tags:
+                if update_tag.startswith(before_tag_key):
+                    preserve = False
+            if preserve:
+                update_tags.append(before_tag)
+        update_tags.sort()
+
+        if before_tags == update_tags:
+            print("skipped")
+        else:
+            if dryrun:
+                print(f"[dryrun] update_tags: {update_tags}")
+            else:
+                try:
+                    res = api.Tag.update(
+                        datadogs_host, tags=update_tags, source="Users")
+                    print(f"updated: {res}")
+                except Exception as e:
+                    raise e
+
+
+if __name__ == '__main__':
+    args = arg_parse()
+    main(args.filter, args.conf_yaml, args.dryrun)
